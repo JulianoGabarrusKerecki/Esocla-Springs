44362432ba2e611b7b4ff2d3c4e10b012e18ed44
Juliano Gabarrus kerecki <Julianogke@hotmail.com>
2020-06-16 09:20:00 -0300

    First commit

---
+++ pastelGradel/src/main/java/kercki/juliano/pastelGradel/PastelGradelApplication.java
@@ -0,0 +1,13 @@
+package kercki.juliano.pastelGradel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastelGradelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PastelGradelApplication.class, args);
+	}
+
+}
---
+++ pastelGradel/src/test/java/kercki/juliano/pastelGradel/PastelGradelApplicationTests.java
@@ -0,0 +1,13 @@
+package kercki.juliano.pastelGradel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastelGradelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
