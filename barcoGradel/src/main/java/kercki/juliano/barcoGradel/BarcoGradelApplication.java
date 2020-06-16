bd4add5960e48f5159cdd044438437a106d917bb
Juliano Gabarrus kerecki <Julianogke@hotmail.com>
2020-06-16 09:10:01 -0300

    First commit

---
+++ barcoGradel/src/main/java/kercki/juliano/barcoGradel/BarcoGradelApplication.java
@@ -0,0 +1,13 @@
+package kercki.juliano.barcoGradel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarcoGradelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarcoGradelApplication.class, args);
+	}
+
+}
---
+++ barcoGradel/src/test/java/kercki/juliano/barcoGradel/BarcoGradelApplicationTests.java
@@ -0,0 +1,13 @@
+package kercki.juliano.barcoGradel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarcoGradelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
