b09adf60fc265264bdef8fddd6ef1e6909fa222e
Juliano Gabarrus kerecki <Julianogke@hotmail.com>
2020-06-16 09:19:03 -0300

    first commit

---
+++ motoGradel/src/main/java/kercki/juliano/motoGradel/MotoGradelApplication.java
@@ -0,0 +1,13 @@
+package kercki.juliano.motoGradel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotoGradelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotoGradelApplication.class, args);
+	}
+
+}
---
+++ motoGradel/src/test/java/kercki/juliano/motoGradel/MotoGradelApplicationTests.java
@@ -0,0 +1,13 @@
+package kercki.juliano.motoGradel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotoGradelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
