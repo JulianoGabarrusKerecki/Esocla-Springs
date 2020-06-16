cc49cdd6734303e63b2596ee8703998c4fba1b35
Juliano Gabarrus kerecki <Julianogke@hotmail.com>
2020-06-16 09:15:09 -0300

    BRAIL FIRST COMMIT

---
+++ bicicletaGradel/src/main/java/kercki/juliano/bicicletaGradel/BicicletaGradelApplication.java
@@ -0,0 +1,13 @@
+package kercki.juliano.bicicletaGradel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BicicletaGradelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BicicletaGradelApplication.class, args);
+	}
+
+}
---
+++ bicicletaGradel/src/test/java/kercki/juliano/bicicletaGradel/BicicletaGradelApplicationTests.java
@@ -0,0 +1,13 @@
+package kercki.juliano.bicicletaGradel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BicicletaGradelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
